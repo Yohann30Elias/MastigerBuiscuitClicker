59c8de5d062e417d02cf092814b7fc535b81a575
Yohann30Elias <yriond30@gmail.com>
2025-04-09 11:04:17 +0200

    init

---
+++ be/src/test/java/ch/zh/yr/be/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zh.yr.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
